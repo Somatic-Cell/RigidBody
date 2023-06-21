e0f4657ac5c799fee10c0cada55701425aad8bc1
soma-yokota <soma.yokota@fj.ics.keio.ac.jp>
2023-06-22 06:25:41 +0900

    first commit

---
+++ Assets/Course/Scripts/Analytical.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Course
+{
+    public class Analytical : MonoBehaviour
+    {
+        [SerializeField] protected Vector3 initPos = new Vector3(0f, 0f, 0f);       // 初期位置
+        [SerializeField] protected Vector3 initVel = new Vector3(10f, 10f, 0f);     // 初期速度
+        [SerializeField] protected Vector3 gravity = new Vector3(0f, -9.8f, 0f);    // 重力加速度
+
+        private Vector3 position;   // 現在位置
+
+        // 最初に一度呼ばれるメソッド．
+        void Start()
+        {
+            // メンバ変数の初期化．
+            position = initPos;
+
+            // アタッチしているオブジェクトの位置を初期化．
+            this.transform.position = position;
+        }
+
+        // FixedUpdate は物理演算を行う際によく用いられる．
+		// とりあえず固定の周期で動いてくれるメソッドと思えばいい．
+        // 詳細はこちら：https://docs.unity3d.com/ja/2021.1/ScriptReference/MonoBehaviour.FixedUpdate.html
+        void FixedUpdate()
+        {
+            float time = Time.time;     // シミュレーション開始からの経過時間を取得
+            position = UpdatePos(time);
+
+            // アタッチしているオブジェクトの位置を更新．
+            this.transform.position = position;
+        }
+
+        // 現在の時刻から，現在位置を求める
+        private Vector3 UpdatePos(float t /* = 現在時刻*/)
+        {
+            Vector3 pos = Vector3.zero;
+            /* pos  = ???; （ここに解析解による位置更新を実装）*/
+            return pos;
+        }
+        
+    }
+}
---
+++ Assets/Reference/Scripts/Numerical.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Reference
+{
+    public class Numerical : MonoBehaviour
+    {
+        [SerializeField] protected Vector3 initPos = new Vector3(0f, 0f, 0f);       // 初期位置
+        [SerializeField] protected Vector3 initVel = new Vector3(10f, 10f, 0f);     // 初期速度
+        [SerializeField] protected Vector3 gravity = new Vector3(0f, -9.8f, 0f);    // 重力加速度
+
+        private Vector3 position;       // 現在の位置
+        private Vector3 velocity;       // 現在の速度
+        private Vector3 acceleration;   // 現在の加速度
+
+        // 最初に一度呼ばれるメソッド．
+        void Start()
+        {
+            // メンバ変数の初期化．
+            position = initPos;
+            velocity = initVel;
+            acceleration = gravity;
+
+            // アタッチしているオブジェクトの位置を初期化．
+            this.transform.position = position;
+        }
+
+        // FixedUpdate は物理演算を行う際によく用いられる．
+        // とりあえず固定の周期で動いてくれるメソッドと思えばいい．
+        // 詳細はこちら：https://docs.unity3d.com/ja/2021.1/ScriptReference/MonoBehaviour.FixedUpdate.html
+        void FixedUpdate()
+        {
+            float dt = Time.deltaTime;
+
+            position = velocity * dt + position;
+            acceleration = gravity;
+			//if (position.y < 0f)
+			//{
+			//	acceleration += Mathf.Abs(position.y) * new Vector3(0f, 1000f, 0f);
+			//}
+			velocity = acceleration * dt + velocity;
+
+            // アタッチしているオブジェクトの位置を更新．
+            this.transform.position = position;
+        }
+    }
+}
